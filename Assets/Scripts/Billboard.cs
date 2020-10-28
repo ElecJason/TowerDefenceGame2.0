abdb5b3da6f40928cb7ba2917716afef89b2ca3b
Jason Dikken <electronicjason.dikken@gmail.com>
2020-10-28 09:19:27 +0100

    Towers

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -4,18 +4,9 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    [SerializeField] private Transform cam;
-
-    private void Awake()
-    {
-        if(cam = null)
-        {
-            cam = Camera.main.transform;
-        }
-    }
     private void LateUpdate()
     {
-
+        transform.LookAt(transform.position + Camera.main.transform.forward);
     }
 
 }
